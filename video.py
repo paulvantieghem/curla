463c5176a518264083cc3a7bac0331df7538d2e1
Paul van Tieghem <paul.vantieghem@icloud.com>
2023-04-05 11:40:47 +0200

    [CURL] Copied necessary files from the original CURL repository to be integrated with the Carla code

---
+++ video.py
@@ -0,0 +1,45 @@
+# This piece of code was copied/modified from the following source:
+#
+#    Title: CURL: Contrastive Unsupervised Representation Learning for Sample-Efficient Reinforcement Learning
+#    Author: Laskin, Michael and Srinivas, Aravind and Abbeel, Pieter
+#    Date: 2020
+#    Availability: https://github.com/MishaLaskin/curl
+
+import imageio
+import os
+import numpy as np
+
+
+class VideoRecorder(object):
+    def __init__(self, dir_name, height=256, width=256, camera_id=0, fps=30):
+        self.dir_name = dir_name
+        self.height = height
+        self.width = width
+        self.camera_id = camera_id
+        self.fps = fps
+        self.frames = []
+
+    def init(self, enabled=True):
+        self.frames = []
+        self.enabled = self.dir_name is not None and enabled
+
+    def record(self, env):
+        if self.enabled:
+            try:
+                frame = env.render(
+                    mode='rgb_array',
+                    height=self.height,
+                    width=self.width,
+                    camera_id=self.camera_id
+                )
+            except:
+                frame = env.render(
+                    mode='rgb_array',
+                )
+    
+            self.frames.append(frame)
+
+    def save(self, file_name):
+        if self.enabled:
+            path = os.path.join(self.dir_name, file_name)
+            imageio.mimsave(path, self.frames, fps=self.fps)
