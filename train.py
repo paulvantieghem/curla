a9ba782c5bc2bad681f0ca46849e7b785b704c8e
Paul van Tieghem <paul.vantieghem@icloud.com>
2023-04-11 21:36:15 +0200

    Small correction

--- train.py
+++ train.py
@@ -197,7 +197,8 @@ def main():
     + str(args.batch_size) + '-s' + str(args.seed)  + '-' + args.encoder_type
     args.work_dir = os.path.join(args.work_dir, exp_name)
     utils.make_dir(args.work_dir)
-    global video_dir = utils.make_dir(os.path.join(args.work_dir, 'video'))
+    global video_dir 
+    video_dir = utils.make_dir(os.path.join(args.work_dir, 'video'))
     model_dir = utils.make_dir(os.path.join(args.work_dir, 'model'))
     buffer_dir = utils.make_dir(os.path.join(args.work_dir, 'buffer'))
 
