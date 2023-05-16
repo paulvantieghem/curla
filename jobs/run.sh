47d2bfc94ce804132785a62ef8a09d572efa13ff
Paul van Tieghem <paul.vantieghem@icloud.com>
2023-05-16 14:25:46 +0200

    Update run.sh

--- jobs/run.sh
+++ jobs/run.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 source /home/.bashrc
 
+# Check if CARLA_SERVER_PORT is free
+for port in "$4" "$((4+1))" "$((4+2))"; do
+    if lsof -i ":$port" >/dev/null 2>&1; then
+        echo "Port $port is in use by the following process(es):"
+        lsof -i ":$port" | awk 'NR>1 {print "  PID:", $2, "Process name:", $1}'
+    else
+        echo "Port $port is free."
+    fi
+done
+
+# Check if CARLA_TM_PORT is free
+for port in "$6"; do
+    if lsof -i ":$port" >/dev/null 2>&1; then
+        echo "Port $port is in use by the following process(es):"
+        lsof -i ":$port" | awk 'NR>1 {print "  PID:", $2, "Process name:", $1}'
+    else
+        echo "Port $port is free."
+    fi
+done
+
 echo "[$(date +%H:%M:%S)] Starting training"
 conda activate curla
 python -u "$@"
