8e4e302e9944cf12d7dd11fcc35155a92f266e35
PaulvanT <43028370+PaulvanT@users.noreply.github.com>
2023-05-16 15:47:13 +0200

    Traffic Manager port experiment

--- jobs/run.sh
+++ jobs/run.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 source /home/.bashrc
 
-# Check if CARLA_SERVER_PORT is free
-for port in "$5" "$(( $5 + 1 ))" "$(( $5 + 2 ))"; do
-    if lsof -i ":$port" >/dev/null 2>&1; then
-        echo "Port $port is in use by the following process(es):"
-        lsof -i ":$port" | awk 'NR>1 {print "  PID:", $2, "Process name:", $1}'
-    else
-        echo "Port $port is free."
-    fi
-done
-
-# Check if CARLA_TM_PORT is free
-for port in "$7"; do
-    if lsof -i ":$port" >/dev/null 2>&1; then
-        echo "Port $port is in use by the following process(es):"
-        lsof -i ":$port" | awk 'NR>1 {print "  PID:", $2, "Process name:", $1}'
-    else
-        echo "Port $port is free."
-    fi
-done
-
 echo "[$(date +%H:%M:%S)] Starting training"
 conda activate curla
 python -u "$@"
