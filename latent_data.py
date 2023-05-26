421a744d8b644f520f8913d2938736a3be2bb4d1
Paul van Tieghem <paul.vantieghem@icloud.com>
2023-05-26 18:44:01 +0200

    t-SNE visualization of the laten space scipt

--- latent_data.py
+++ latent_data.py
@@ -4,12 +4,14 @@ import torch
 import numpy as np
 import json
 import carla
+from tqdm import tqdm
 
 import utils
 from augmentations import make_augmentor
 import carla_env
 from video import VideoRecorder
 from train import make_agent
+from latent_episodes import *
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -17,83 +19,6 @@ def parse_args():
     parser.add_argument('--model_step', default=1_000_000, type=int)
     args = parser.parse_args()
     return args
-
-def make_env(args):
-
-    # Initialize the CARLA environment
-    env = carla_env.CarlaEnv(args.carla_town, args.max_npc_vehicles, 
-                   args.desired_speed, args.max_stall_time, args.stall_speed, args.seconds_per_episode,
-                   args.fps, 2000, 8000, args.env_verbose, args.camera_image_height, args.camera_image_width, 
-                   args.fov, args.cam_x, args.cam_y, args.cam_z, args.cam_pitch,
-                   args.lambda_r1, args.lambda_r2, args.lambda_r3, args.lambda_r4, args.lambda_r5)
-    
-    # Set the random seed and reset
-    env.seed(args.seed)
-    env.reset()
-
-    # Wrap CarlaEnv in FrameStack class to stack several consecutive frames together
-    env = utils.FrameStack(env, k=args.frame_stack)
-
-    return env
-
-def run_episode(env, agent, augmentor, step, experiment_dir_path, weather_name):
-        
-        # Initializations
-        path = os.path.join(experiment_dir_path, 'latent_data')
-        if not os.path.exists(path):
-            os.mkdir(path)
-        video = VideoRecorder(path, env.fps)
-
-        latent_value_dict = {}
-
-        # Run evaluation loop
-        obs = env.reset()
-        video.init(enabled=True)
-        done = False
-        step = 0
-        while not done:
-
-            # Perform anchor augmentation
-            obs = augmentor.anchor_augmentation(obs)
-
-            # Get the latent representation
-            with torch.no_grad():
-                gpu_obs = torch.FloatTensor(obs).to('cuda')
-                gpu_obs = gpu_obs.unsqueeze(0)
-                latent_representation = agent.actor.encoder(gpu_obs)
-
-            # Sample action from agent
-            with utils.eval_mode(agent):
-                action = agent.sample_action(obs)
-
-            # Get the values
-            with torch.no_grad():
-                gpu_action = torch.FloatTensor(action).to('cuda')
-                gpu_action = gpu_action.unsqueeze(0)
-                q1 = agent.critic.Q1(latent_representation, gpu_action)
-                q2 = agent.critic.Q2(latent_representation, gpu_action)
-
-            # Save the latent representation and the values
-            latent_representation = list(latent_representation.detach().cpu().numpy().squeeze())
-            latent_representation = [float(x) for x in latent_representation]
-            q1 = q1.detach().cpu().numpy()
-            q2 = q2.detach().cpu().numpy()
-            q = float(np.min([q1, q2], axis=0))
-            latent_value_dict[step] = {'latent_representation': latent_representation, 'q_value': q}
-
-            # Take step in environment
-            obs, reward, done, _ = env.step(action)
-            step += 1
-
-            # Administration and logging
-            video.record(env)
-        
-        # Save the video
-        video.save(f'{weather_name}.mp4')
-
-        # Save latent_value_dict dictionary to file
-        with open(os.path.join(path, f'{weather_name}.json'), 'w') as f:
-            json.dump(latent_value_dict, f)
 
 def main():
 
@@ -105,48 +30,79 @@ def main():
     # Set a fixed random seed for reproducibility across weather presets.
     args.seed = 0
 
-    # Set shorter episode time
-    args.seconds_per_episode = 20
-
     # Random seed
     utils.set_seed_everywhere(args.seed)
 
     # Anchor/target data augmentor
     camera_image_shape = (args.camera_image_height, args.camera_image_width)
     augmentor = make_augmentor(args.augmentation, camera_image_shape)
-
-    # Perform the same episode for each weather preset
-    WEATHER_PRESETS =  {'ClearNoon': carla.WeatherParameters.ClearNoon,
-                        'ClearSunset': carla.WeatherParameters.ClearSunset, 
-                        'CloudyNoon': carla.WeatherParameters.CloudyNoon, 
-                        'CloudySunset': carla.WeatherParameters.CloudySunset, 
-                        'WetNoon': carla.WeatherParameters.WetNoon, 
-                        'WetSunset': carla.WeatherParameters.WetSunset, 
-                        'MidRainSunset': carla.WeatherParameters.MidRainSunset,
-                        'MidRainyNoon': carla.WeatherParameters.MidRainyNoon}
     
-    # Thanks to the fixed random seed, the same episode will be performed for each 
-    # weather preset if the CARLA server process is killed in between
-    for weather_name in WEATHER_PRESETS:
+    # Load the CURL model
+    action_shape = (2,)
+    obs_shape = (3*args.frame_stack, args.augmented_image_height, args.augmented_image_width)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    agent = make_agent(obs_shape, action_shape, args, device, augmentor)
+    model_dir_path = os.path.join(args.experiment_dir_path, 'model')
+    agent.load(model_dir_path, str(args.augmentation), str(args.model_step))
 
-        # Launch the CARLA server and load the model
-        env = make_env(args)
-        env.weather_presets = [WEATHER_PRESETS[weather_name],]
-        print(env.weather_presets)
-        print(f'Running episode for weather preset {weather_name}...')
-        action_shape = env.action_space.shape
-        pre_aug_obs_shape = env.observation_space.shape
-        obs_shape = (3*args.frame_stack, args.augmented_image_height, args.augmented_image_width)
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        agent = make_agent(obs_shape, action_shape, args, device, augmentor)
-        model_dir_path = os.path.join(args.experiment_dir_path, 'model')
-        agent.load(model_dir_path, str(args.augmentation), str(args.model_step))
+    # Load replay buffer
+    replay_buffer = CustomReplayBuffer(obs_shape=obs_shape, action_shape=action_shape, capacity=10_000)
+    replay_buffer.load('./latent_data/replay_buffer.npz')
 
-        # Run the episode
-        run_episode(env, agent, augmentor, args.model_step, args.experiment_dir_path, weather_name)
+    # Initialize dictionary to store latent representations and values
+    latent_value_dict = {}
 
-        # Deactivate the environment (kills the CARLA server)
-        env.deactivate()
+    # Loop over replay buffer and save latent representation and values
+    for i in tqdm(range(replay_buffer.obses.shape[0])):
+
+        # Get data from replay buffer
+        obs = replay_buffer.obses[i]
+        action = replay_buffer.actions[i]
+        reward = float(replay_buffer.rewards[i])
+        done = int(replay_buffer.dones[i])
+        speed = int(replay_buffer.speeds[i])
+        weather_preset_idx = int(replay_buffer.weather_preset_idxs[i])
+
+        # Get the weather preset name
+        weather_preset_idx
+
+        # Perform anchor augmentation
+        obs = augmentor.anchor_augmentation(obs)
+
+        # Get the latent representation
+        with torch.no_grad():
+            gpu_obs = torch.FloatTensor(obs).to('cuda')
+            gpu_obs = gpu_obs.unsqueeze(0)
+            latent_representation = agent.actor.encoder(gpu_obs)
+
+        # Get the models own action (OPTIONAL)
+        # with utils.eval_mode(agent):
+        #     action = agent.sample_action(obs)
+        
+        # Get the values
+        with torch.no_grad():
+            gpu_action = torch.FloatTensor(action).to('cuda')
+            gpu_action = gpu_action.unsqueeze(0)
+            q1 = agent.critic.Q1(latent_representation, gpu_action)
+            q2 = agent.critic.Q2(latent_representation, gpu_action)
+
+        # Save the latent representation and the values
+        latent_representation = list(latent_representation.detach().cpu().numpy().squeeze())
+        latent_representation = [float(x) for x in latent_representation]
+        q1 = q1.detach().cpu().numpy()
+        q2 = q2.detach().cpu().numpy()
+        q = float(np.min([q1, q2], axis=0))
+        latent_value_dict[i] = {'latent_representation': latent_representation, 
+                                'q_value': q,
+                                'reward': reward,
+                                'speed': speed,
+                                'weather_preset_idx': weather_preset_idx}
+
+    # Save latent_value_dict dictionary to file
+    augmentation_name = str(args.augmentation)
+    with open(os.path.join('latent_data', f'{augmentation_name}.json'), 'w') as f:
+        json.dump(latent_value_dict, f)
+
 
 if __name__ == "__main__":
     main()
--- latent_viz.py
+++ latent_viz.py
@@ -5,6 +5,18 @@ import numpy as np
 import argparse
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+import matplotlib
+
+import warnings
+warnings.filterwarnings(action='ignore')
+
+WEATHER_PRESETS =  {0: 'ClearNoon',
+                    1: 'ClearSunset', 
+                    2: 'CloudyNoon', 
+                    3: 'CloudySunset', 
+                    4: 'WetNoon', 
+                    5: 'WetSunset', 
+                    6: 'MidRainSunset'}
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -22,33 +34,39 @@ def plot_latent_tsne(file_path, file_name):
     # Get the latent representations and q values in the right format
     representations = []
     q_values = []
+    weather_presets = []
     for step in latent_value_dict.keys():
         representations.append(np.array(latent_value_dict[step]['latent_representation']))
         q_values.append(latent_value_dict[step]['q_value'])
+        weather_presets.append(latent_value_dict[step]['weather_preset_idx'])
     assert len(representations) == len(q_values)
-    representations = np.array(representations)
-    q_values = np.array(q_values)
-
+    representations = np.array(representations, dtype=np.float32)
+    q_values = np.array(q_values, dtype=np.float32)
+    weather_presets = np.array(weather_presets, dtype=np.uint8)
 
     # Perform t-SNE on the latent representations
     tsne = TSNE(n_components=2)
     vec_2d = tsne.fit_transform(representations)
 
-
     # Visualize latent representations in function of Q-values
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6), dpi=300)
-    scatter = ax1.scatter(vec_2d[:,0], vec_2d[:,1], c=q_values, cmap='viridis', s=30)
-    cbar = plt.colorbar(scatter, ax=ax1)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6), dpi=300)
+    scatter = ax1.scatter(vec_2d[:,0], vec_2d[:,1], c=q_values, cmap='viridis', s=10)
+    ax1.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
+    cbar = plt.colorbar(scatter, ax=ax1, location='left')
+    cbar.ax.set_yticklabels(np.linspace(int(np.min(q_values)), int(np.max(q_values)), 10, endpoint=False).astype(np.int32))
     cbar.set_label('Q Value')
 
-    # Visualize latent representations in function of steps
-    scatter = ax2.scatter(vec_2d[:,0], vec_2d[:,1], c=np.arange(q_values.shape[0]), cmap='viridis', s=30)
+    # Visualize latent representations in function of weather presets
+    scatter = ax2.scatter(vec_2d[:,0], vec_2d[:,1], c=weather_presets, cmap='tab10', s=10)
+    ax2.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
     cbar = plt.colorbar(scatter, ax=ax2)
-    cbar.set_label('Step')
+    cbar.ax.set_yticklabels(WEATHER_PRESETS.values())
+    cbar.set_label('Weather Preset')
 
     # Save the figure
     title = file_name.split('.')[0]
-    fig.suptitle(f'Weather preset: {title}')
+    fig.suptitle(f'Augmentation: {title}')
+    plt.tight_layout()
     plt.savefig(os.path.join(file_path, f'{title}.png'))
 
 def main():
@@ -63,9 +81,10 @@ def main():
     utils.set_seed_everywhere(args.seed)
 
     # Iterate over every .json file in the latent_data directory
-    for file_name in os.listdir(os.path.join(args.experiment_dir_path, 'latent_data')):
-        if file_name.endswith('.json'):
-            file_path = os.path.join(args.experiment_dir_path, 'latent_data')
+    for file_name in os.listdir('./latent_data'):
+        if file_name.endswith('.json') and str(args.augmentation) in file_name:
+            file_path = './latent_data'
+            print(f'Plotting latent t-SNE for {file_name}')
             plot_latent_tsne(file_path, file_name)
 
 if __name__ == '__main__':
