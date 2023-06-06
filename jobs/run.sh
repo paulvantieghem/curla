42b20c573e6d77ffc0de36d7cf72e446b482e894
PaulvanT <43028370+PaulvanT@users.noreply.github.com>
2023-06-06 12:41:43 +0200

    cleanup

--- jobs/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /home/.bashrc
-
-echo "[$(date +%H:%M:%S)] Starting training"
-conda activate curla
-python -u "$@"
-conda deactivate
-echo "[$(date +%H:%M:%S)] Training finished"
