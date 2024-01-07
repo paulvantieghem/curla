dc34f154cc3720e1a908ab915534971972f24a8b
PaulvanT <43028370+PaulvanT@users.noreply.github.com>
2024-01-07 18:26:35 +0100

    Adapted weight_init method to handle layers without bias

--- curl_sac.py
+++ curl_sac.py
@@ -39,12 +39,16 @@ def weight_init(m):
     """Custom weight init for Conv2D and Linear layers."""
     if isinstance(m, nn.Linear):
         nn.init.orthogonal_(m.weight.data)
-        m.bias.data.fill_(0.0)
+        # Assert that there is a bias before applying the delta-orthogonal init
+        if m.bias is not None:
+            m.bias.data.fill_(0.0)
     elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
         # delta-orthogonal init from https://arxiv.org/pdf/1806.05393.pdf
         assert m.weight.size(2) == m.weight.size(3)
         m.weight.data.fill_(0.0)
-        m.bias.data.fill_(0.0)
+        # Assert that there is a bias before applying the delta-orthogonal init
+        if m.bias is not None:
+            m.bias.data.fill_(0.0)
         mid = m.weight.size(2) // 2
         gain = nn.init.calculate_gain('relu')
         nn.init.orthogonal_(m.weight.data[:, :, mid, mid], gain)
